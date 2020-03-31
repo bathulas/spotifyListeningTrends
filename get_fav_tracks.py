8f42a9bbade430096a807a7aa8b90cb0ab8a3770
bathulas <sreenivasbathula1@gmail.com>
2020-03-31 02:17:03 -0400

    Update get_fav_tracks.py
    minor fixes

--- get_fav_tracks.py
+++ get_fav_tracks.py
@@ -7,7 +7,7 @@ offset = 0
 limit = 50
 total_tracks = []
 
-def get_fav_tracks(url, offset, limit):
+def get_data(url, offset, limit):
     token = "{token}"
 #Your token goes here
     headers = {
@@ -23,7 +23,7 @@ while len(response["items"]) > 0:
     time.sleep(1)
     offset += limit
     total.append(response)
-    response = get("https://api.spotify.com/v1/me/tracks", offset, limit)
+    response = get_data("https://api.spotify.com/v1/me/tracks", offset, limit)
 # Get audio features of favorite tracks
 track_ids = [[(track['added_at'],track['track']['id']) for track in x['items']]   for x in total]
 id_strings = ["%2C".join([x[1] for x in id_list_]) for id_list_ in track_ids]
@@ -32,5 +32,5 @@ track_features_ = []
 while (counter+1) < len(id_strings):
     counter += 1
     time.sleep(0.6)
-    resp = get("https://api.spotify.com/v1/audio-features?ids=" + id_strings[counter],0,0)
+    resp = get_data("https://api.spotify.com/v1/audio-features?ids=" + id_strings[counter],0,0)
     track_features_.append(resp)
